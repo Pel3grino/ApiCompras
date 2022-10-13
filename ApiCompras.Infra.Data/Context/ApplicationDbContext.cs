918ead68d1564970a4f605b61b3b277f4c8d49be
Pierre dos S Romano <pel3grino@gmail.com>
2022-10-13 02:39:55 -0300

    Criado o DbContext e feito o mapeamento da classe Person

---
+++ ApiCompras.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ApiCompras.Domain.Entitie;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCompras.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+        }
+        public DbSet<Person> People { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
