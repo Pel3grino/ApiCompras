54a81a2313b2291c6d961b814e96665b22dff009
Pierre dos S Romano <pel3grino@gmail.com>
2022-10-13 16:53:04 -0300

    Implementado DTO para classe Person e validação do DTO

---
+++ ApiCompras.Aplication/DTO/PersonDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCompras.Application.DTO
+{
+    public class PersonDto
+    {
+        public Guid Id { get; set; }
+        public int Code { get;  set; }
+        public string Name { get;  set; }
+        public string Document { get;  set; }
+        public string? Phone { get;  set; }
+    }
+}
---
+++ ApiCompras.Aplication/DTO/Validation/PersonDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCompras.Application.DTO.Validation
+{
+    public class PersonDtoValidator : AbstractValidator<PersonDto>
+    {
+        public PersonDtoValidator()
+        {
+            RuleFor(x => x.Document)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Documento deve ser informado!");
+            RuleFor(x => x.Name)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Nome deve ser informado!");
+            RuleFor(x => x.Phone)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Telfone deve ser informado!");
+
+        }
+    }
+}
